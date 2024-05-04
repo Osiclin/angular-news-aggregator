9168b533f06ba6f7463385544938b1ee1ec3c39a
Osita Ezeigbo <osiclin@yahoo.com>
2024-05-04 17:21:07 +0100

    Create article page and integrate service

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,12 +1,34 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
+import { ArticlesService } from './services/articles.service';
+import { Article, FetchedArticle } from './types/articles';
+import { of, Observable } from 'rxjs';
+import { HttpClientTestingModule, HttpTestingController } from '@angular/common/http/testing';
+import { articles as dummyArticles } from '../mocks/articles';
 
 describe('AppComponent', () => {
+  let component: AppComponent;
+  let fixture: ComponentFixture<AppComponent>;
+  let mockArticlesService: Partial<ArticlesService>;
+  let httpTestingController: HttpTestingController
+
   beforeEach(async () => {
+    mockArticlesService = {
+      getArticles: jest.fn()
+    } as unknown as ArticlesService;
     await TestBed.configureTestingModule({
-      imports: [AppComponent],
+      imports: [AppComponent, HttpClientTestingModule],
+      providers: [{ provide: ArticlesService, useValue: mockArticlesService }]
     }).compileComponents();
+
+    fixture = TestBed.createComponent(AppComponent);
+    component = fixture.componentInstance;
+    httpTestingController = TestBed.inject(HttpTestingController);
   });
+
+  afterEach(() => {
+    httpTestingController.verify()
+  })
 
   it('should create the app', () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -14,16 +36,34 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have the 'news-aggregator' title`, () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.componentInstance;
-    expect(app.title).toEqual('news-aggregator');
+  it('should fetch articles on initialization', () => {
+    const articles: Article[] = dummyArticles.articles
+
+    // Mocking the getArticles method using spyOn
+    jest.spyOn(mockArticlesService, 'getArticles')
+      .mockReturnValue(of({ articles, totalResults: articles.length }) as Observable<FetchedArticle>);
+
+    fixture.detectChanges();
+
+    expect(mockArticlesService.getArticles).toHaveBeenCalled();
+    expect(component.isLoading).toBeFalsy();
+    expect(component.articles).toEqual(articles);
+    expect(component.totalResults).toEqual(articles.length);
   });
 
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('h1')?.textContent).toContain('Hello, news-aggregator');
+  it('should change page', () => {
+    const event = { first: 10, rows: 10, page: 2 };
+    const selectedPage = event.first / event.rows + 1;
+    const articles: Article[] = dummyArticles.articles
+
+    // Mocking the getArticles method using spyOn
+    jest.spyOn(mockArticlesService, 'getArticles')
+      .mockReturnValue(of({ articles, totalResults: articles.length }) as Observable<FetchedArticle>);
+
+    component.changePage(event);
+
+    expect(component.first).toEqual(event.first);
+    expect(component.page).toEqual(selectedPage);
+    expect(mockArticlesService.getArticles).toHaveBeenCalledWith(selectedPage);
   });
 });
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,54 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Article } from './types/articles';
+import { ArticlesService } from './services/articles.service';
+import { PaginatorModule } from 'primeng/paginator';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet, PaginatorModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
 export class AppComponent {
-  title = 'news-aggregator';
+  title: string = 'News Aggregator';
+  articles: Article[] = []
+  isLoading: boolean = true
+  page: number = 1
+  limit: number = 10
+  first: number = 0
+  totalResults?: number = 0
+
+  constructor(
+    private articleService: ArticlesService,
+  ) { }
+
+  ngOnInit(): void {
+    this.fetchArticles(this.page)
+  }
+
+  fetchArticles(page: number) {
+    this.isLoading = true
+    this.articleService
+      .getArticles(page)
+      .subscribe(
+        (articles) => {
+          this.articles = articles.articles
+          this.totalResults = articles.totalResults
+        },
+        (error) => console.error(error),
+        () => this.isLoading = false
+      )
+  }
+
+  changePage(event: any) {
+    const { first, rows, page } = event
+    if ((page + 1) === this.page) return
+    this.first = first;
+    const selectedPage = first / rows + 1;
+    this.page = selectedPage
+    this.fetchArticles(selectedPage)
+  }
 }
